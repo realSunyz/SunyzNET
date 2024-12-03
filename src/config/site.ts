7a6cd0f4de294816834d60bdd9e98e604b1ff2d8
Seven <i@sunyz.net>
2024-12-03 12:49:12 +0800

    [+] Update site descriptions

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ import type { SidebarNavItem, SiteConfig } from "@/types";
 export const siteConfig: SiteConfig = {
   name: "Sunyz Network",
   description:
-    "Sunyz Experimental Global Network, aka. SunyzNET, AS150289",
+    "Sunyz Network (AS150289, aka. SunyzNET) is an experimental IPv6-only networking project focused on educational and research purposes, operated by Yanzheng SUN.",
   url: "https://sunyz.net",
   ogImage: "https://astro-nomy.vercel.app/og.jpg",
   links: {
