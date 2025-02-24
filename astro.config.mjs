547a80ac7b5df858e9f041acfc1ed24c71eab3fc
Seven SUN <i@sunyz.net>
2025-02-24 13:25:30 +0800

    fix: update security headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,16 +15,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  server: {
-    headers: {
-      "Content-Security-Policy": "default-src https: data: 'unsafe-inline' 'unsafe-eval'",
-      "Access-Control-Allow-Origin": "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.sunyz.net; style-src 'self' 'unsafe-inline' https://cdn.sunyz.net https://fonts.googleapis.com; img-src 'self' blob: data: https://cdn.sunyz.net; font-src 'self' https://cdn.sunyz.net https://fonts.gstatic.com; object-src 'none'; base-uri 'self'; form-action 'self'; frame-src 'none'; frame-ancestors 'none';",
-      "Permissions-Policy": "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()",
-      "Referrer-Policy": "no-referrer-when-downgrade",
-      "X-Content-Type-Options": "nosniff",
-      "X-Frame-Options": "DENY",
-    },
-  },
   output: "server",
   adapter: cloudflare(),
 });
