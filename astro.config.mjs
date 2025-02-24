bbe3064cf9dc843737c204f94a0bdb813305888f
Seven SUN <i@sunyz.net>
2025-02-24 12:14:22 +0800

    feat: add security headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,39 @@ import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.sunyz.net;
+    style-src 'self' 'unsafe-inline' https://cdn.sunyz.net https://fonts.googleapis.com;
+    img-src 'self' blob: data: https://cdn.sunyz.net;
+    font-src 'self' https://cdn.sunyz.net https://fonts.gstatic.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-src 'none';
+    frame-ancestors 'none';
+`
+
 export default defineConfig({
   site: "https://sunyz.net",
   integrations: [
     icon(),
     sitemap(),
     react(),
-    tailwind({  
+    tailwind({
       applyBaseStyles: false,
     }),
   ],
+  server: {
+    headers: {
+      "X-Frame-Options": "DENY",
+      "X-Content-Type-Options": "nosniff",
+      "Referrer-Policy": "no-referrer-when-downgrade",
+      "Permissions-Policy": "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()",
+      "Access-Control-Allow-Origin": "https://sunyz.net",
+      "Content-Security-Policy": cspHeader.replace(/\n/g, ''),
+    },
+  },
   output: "server",
   adapter: cloudflare(),
 });
