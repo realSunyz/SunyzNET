bcf6943cdc5acc1e3f01c847fd5e73a07ab81aab
Seven SUN <i@sunyz.net>
2025-02-24 12:20:20 +0800

    fix: remove headers from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,6 @@ import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.sunyz.net;
-    style-src 'self' 'unsafe-inline' https://cdn.sunyz.net https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://cdn.sunyz.net;
-    font-src 'self' https://cdn.sunyz.net https://fonts.gstatic.com;
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-src 'none';
-    frame-ancestors 'none';
-`
-
 export default defineConfig({
   site: "https://sunyz.net",
   integrations: [
@@ -28,16 +15,6 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
   ],
-  server: {
-    headers: {
-      "X-Frame-Options": "DENY",
-      "X-Content-Type-Options": "nosniff",
-      "Referrer-Policy": "no-referrer-when-downgrade",
-      "Permissions-Policy": "accelerometer=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()",
-      "Access-Control-Allow-Origin": "https://sunyz.net",
-      "Content-Security-Policy": cspHeader.replace(/\n/g, ''),
-    },
-  },
   output: "server",
   adapter: cloudflare(),
 });
