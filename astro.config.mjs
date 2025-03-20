01c5f1ca7737d74cb628a7c4edc70cf2534e1fe5
Seven SUN <i@sunyz.net>
2025-03-20 10:41:45 +0800

    fix: remove astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 import icon from "astro-icon";
@@ -11,7 +10,6 @@ export default defineConfig({
   site: "https://sunyz.net",
   integrations: [
     icon(),
-    sitemap(),
     react(),
     tailwind({
       applyBaseStyles: false,
